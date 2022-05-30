c30c4b49b8c601350a859c7222612e3a9e79881e
naftalib <45719650+naftalib@users.noreply.github.com>
2022-05-30 03:27:57 +0300

    working

--- client/js/app.js
+++ client/js/app.js
@@ -18,125 +18,253 @@ const entries = [
     "in":1648587780907,
     "out":1648604220677,
     "license":"M4TM7F"
-  },
-  {"in":1648587780907,"out":1648610590195,"license":"6H31R7"},
-    {"in":1648587840907,"out":1648665752295,"license":"3897R5"},
-    {"in":1648587900907,"out":1648610162289,"license":"9K69VN"},
-    {"in":1648587960907,"out":1648606228707,"license":"PS1FNF"},
-    {"in":1648587960907,"out":1648644498052,"license":"6VEIDP"},
-    {"in":1648588020907,"out":1648599622784,"license":"6LLC1H"},
-    {"in":1648588080907,"out":1648639562630,"license":"PF42ZT"},
-    {"in":1648588080907,"out":1648603520334,"license":"AD6LAX"},
-    {"in":1648588200907,"out":1648654333282,"license":"KMYZDV"},
-    {"in":1648588260907,"out":1648636369515,"license":"KSWZY4"},
-    {"in":1648588260907,"out":1648625477803,"license":"JVCQHU"},
-    {"in":1648588320907,"out":1648660737629,"license":"QGHAVN"},
-    {"in":1648588500907,"out":1648664391909,"license":"QAICR4"},
-    {"in":1648588560907,"out":1648671897050,"license":"KMFSQT"},
-    {"in":1648588620907,"out":1648608611547,"license":"XSANY9"},
-    {"in":1648588740907,"out":1648669745587,"license":"W6BWMR"},
-    {"in":1648588740907,"out":1648652785277,"license":"WTHMAI"},
-    {"in":1648588860907,"out":1648600504093,"license":"ITEBAI"},
-    {"in":1648588860907,"out":1648673110967,"license":"97EJ2R"},
-    {"in":1648588980907,"out":1648614305991,"license":"52GOBK"},
-    {"in":1648589040907,"out":1648661665164,"license":"QHDXCL"},
-    {"in":1648589100907,"out":1648655544160,"license":"OLUPP8"},
-    {"in":1648589100907,"out":1648665141569,"license":"60MUS3"},
-    {"in":1648589160907,"out":1648609664648,"license":"DQNE5V"},
-    {"in":1648589160907,"out":1648673338281,"license":"66V30T"},
-    {"in":1648589220907,"out":1648620887330,"license":"EIQTQ1"},
-    {"in":1648589460907,"out":1648601372178,"license":"YOF4FG"},
-    {"in":1648589460907,"out":1648593280250,"license":"EKT66Q"},
-    {"in":1648589520907,"out":1648633272238,"license":"FK7Y34"},
-    {"in":1648589520907,"out":1648666649674,"license":"FL7HHM"},
-    {"in":1648589580907,"out":1648630056500,"license":"JWE573"},
-    {"in":1648589640907,"out":1648591738977,"license":"E7J0RM"},
-    {"in":1648589640907,"out":1648619029713,"license":"FB2H4H"},
-    {"in":1648589700907,"out":1648616419315,"license":"34KLKX"},
-    {"in":1648589760907,"out":1648671081298,"license":"TWEY11"},
-    {"in":1648589760907,"out":1648634037281,"license":"3FSMVD"},
-    {"in":1648589820907,"out":1648611817646,"license":"2WVYFM"},
-    {"in":1648589820907,"out":1648645706784,"license":"93QW9D"}
+  },           
+  {in: 1653822480403, out: 1653852133351, license: 'TGVSKU'},
+  {in: 1653822480403, out: 1653827798481, license: 'DSTZU3'},
+  {in: 1653822540403, out: 1653842866626, license: 'K0ZCG6'},
+  {in: 1653822540403, out: 1653829794089, license: 'H26SMU'},
+  {in: 1653822600403, out: 1653851196700, license: 'PTMS10'},
+  {in: 1653822600403, out: 1653856655783, license: 'J0ZIXA'},
+  {in: 1653822660403, out: 1653851058566, license: 'HCNGNE'},
+  {in: 1653822720403, out: 1653843890613, license: '30NTPZ'},
+  {in: 1653822720403, out: 1653844323914, license: 'PM6Y8G'},
+  {in: 1653822780403, out: 1653855717957, license: 'FFDXK3'},
+  {in: 1653822780403, out: 1653845167502, license: '6FYI36'},
+  {in: 1653822840403, out: 1653830299592, license: 'I373YO'},
+  {in: 1653822840403, out: 1653826786725, license: 'L3XLWT'},
+  {in: 1653822900403, out: 1653842683328, license: 'NYBJF0'},
+  {in: 1653822960403, out: 1653827296302, license: 'EGSX5Q'},
+  {in: 1653822960403, out: 1653848524929, license: 'FKXN2A'},
+  {in: 1653823020403, out: 1653829389881, license: 'I71D12'},
+  {in: 1653823020403, out: 1653834853725, license: '6XKGAV'},
+  {in: 1653823080403, out: 1653832123396, license: 'VB3JFV'},
+  {in: 1653823140403, out: 1653831245995, license: '848LWR'},
+  {in: 1653823140403, out: 1653836673212, license: 'WL6GXQ'},
+  {in: 1653823200403, out: 1653855933335, license: 'WZBP2W'},
+  {in: 1653823200403, out: 1653827812053, license: 'I89HQ0'},
+  {in: 1653823260403, out: 1653846072434, license: '6CKJEH'},
+  {in: 1653823260403, out: 1653856912060, license: 'RDGB9J'},
+  {in: 1653823380403, out: 1653851004818, license: 'KUZ5SN'},
+  {in: 1653823380403, out: 1653830558790, license: '5OVQBK'},
+  {in: 1653823560403, out: 1653848910495, license: 'GLXESI'},
+  {in: 1653823620403, out: 1653834155224, license: 'YAN1KR'},
+  {in: 1653823680403, out: 1653828669790, license: 'PGPWJ3'},
+  {in: 1653823680403, out: 1653856906355, license: 'J3E8E9'},
+  {in: 1653823740403, out: 1653831237036, license: 'OB613A'},
+  {in: 1653823740403, out: 1653830611121, license: 'U9D7H6'},
+  {in: 1653823800403, out: 1653827308089, license: 'FJ89NE'},
+  {in: 1653823860403, out: 1653843201673, license: 'WHERBE'},
+  {in: 1653823860403, out: 1653835425052, license: 'ZK1WZD'},
+  {in: 1653823920403, out: 1653848220896, license: 'DZI9K0'},
+  {in: 1653823920403, out: 1653836137659, license: '1F9MDB'},
+  {in: 1653824040403, out: 1653854010210, license: 'QG2KGM'},
+  {in: 1653824040403, out: 1653856953385, license: '1I93ZQ'},
+  {in: 1653824100403, out: 1653832383275, license: '72W9Y8'},
+  {in: 1653824100403, out: 1653826990060, license: '9PV5CG'},
+  {in: 1653824160403, out: 1653854701005, license: 'CH15HT'},
+  {in: 1653824340403, out: 1653825491567, license: '7TOJ10'},
+  {in: 1653824340403, out: 1653835171437, license: 'VPXJ4J'},
+  {in: 1653824400403, out: 1653854953140, license: '63L257'},
+  {in: 1653824400403, out: 1653851927983, license: 'ZNBGJ5'},
+  {in: 1653824460403, out: 1653854043497, license: '3Q24NZ'},
+  {in: 1653824520403, out: 1653851432108, license: '2FWCO2'},
+  {in: 1653824520403, out: 1653857519972, license: '5W7EGV'},
+  {in: 1653824640403, out: 1653838254850, license: 'U9TUKL'},
+  {in: 1653824700403, out: 1653854352072, license: '8VY04X'},
+  {in: 1653824700403, out: 1653825312587, license: 'O8DSUI'},
+  {in: 1653824760403, out: 1653844920278, license: 'YFJONQ'},
+  {in: 1653824880403, out: 1653844846520, license: '2Q43XL'},
+  {in: 1653825000403, out: 1653847573018, license: '6QJ7H0'},
+  {in: 1653825000403, out: 1653855575450, license: '39FSZE'},
+  {in: 1653825060403, out: 1653828273767, license: '64SF9D'},
+  {in: 1653825060403, out: 1653841048247, license: '7PLVXZ'},
+  {in: 1653825120403, out: 1653829499143, license: 'VQCPIV'},
+  {in: 1653825180403, out: 1653853042101, license: '084N3X'},
+  {in: 1653825180403, out: 1653826892034, license: 'QRP9CS'},
+  {in: 1653825240403, out: 1653847264983, license: 'KQJC25'},
+  {in: 1653825240403, out: 1653838804205, license: 'XESNYB'},
+  {in: 1653825360403, out: 1653846598498, license: 'MQ2QN3'},
+  {in: 1653825420403, out: 1653831650791, license: 'JE2IPA'},
+  {in: 1653825420403, out: 1653841411238, license: '4ANJF1'},
+  {in: 1653825480403, out: 1653851293947, license: 'XSTR63'},
+  {in: 1653825540403, out: 1653835081219, license: 'ZBG9ZN'},
+  {in: 1653825540403, out: 1653833432943, license: '03ACUA'},
+  {in: 1653825600403, out: 1653856514068, license: 'QC23BI'},
+  {in: 1653825660403, out: 1653836285913, license: 'WTCFA2'},
+  {in: 1653825660403, out: 1653836100390, license: 'OYXA5B'},
+  {in: 1653825720403, out: 1653826891006, license: 'HQI6O7'},
+  {in: 1653825720403, out: 1653836515047, license: 'TAWSIW'},
+  {in: 1653825780403, out: 1653826111507, license: 'TK3O8Z'},
+  {in: 1653825780403, out: 1653827927833, license: 'CIOHAM'},
+  {in: 1653825900403, out: 1653846283081, license: '5M3WK9'},
+  {in: 1653825960403, out: 1653833738460, license: 'SPS3MS'},
+  {in: 1653826020403, out: 1653840042266, license: 'KISYL8'},
+  {in: 1653826020403, out: 1653846702483, license: 'DV8SQA'},
+  {in: 1653826140403, out: 1653852568792, license: 'GTE4AX'},
+  {in: 1653826140403, out: 1653846072185, license: 'WRT59R'},
+  {in: 1653826200403, out: 1653846357493, license: 'V72J2S'},
+  {in: 1653826260403, out: 1653831968099, license: 'M2QLU7'},
+  {in: 1653826320403, out: 1653848343541, license: 'Q4FVPD'},
+  {in: 1653826500403, out: 1653847991241, license: 'UP9IKW'},
+  {in: 1653826560403, out: 1653849669241, license: 'D0Q9KV'},
+  {in: 1653826560403, out: 1653850926252, license: 'AI2WLE'},
+  {in: 1653826620403, out: 1653854566485, license: 'AJMKQX'},
+  {in: 1653826680403, out: 1653834641358, license: 'X9L6I6'},
+  {in: 1653826740403, out: 1653836896570, license: '11336Q'},
+  {in: 1653826740403, out: 1653839910436, license: 'BE4QLI'},
+  {in: 1653827040403, out: 1653837851944, license: 'SUBX2Y'},
+  {in: 1653827100403, out: 1653828885780, license: 'AU54X1'},
+  {in: 1653827400403, out: 1653848055414, license: 'UNS32M'},
+  {in: 1653827400403, out: 1653845013796, license: '6YNT7W'},
+  {in: 1653827460403, out: 1653844759663, license: '37U7X4'},
+  {in: 1653827580403, out: 1653851537134, license: 'TLBP1V'},
+  {in: 1653827580403, out: 1653848885801, license: 'WDXGPU'},
+  
+{in: 1653827700403, out: 1653856427718, license: 'SS91EY'},
+{in: 1653827760403, out: 1653828118822, license: 'EB4OWN'},
+{in: 1653827760403, out: 1653851657064, license: '17925R'},
+{in: 1653827880403, out: 1653850989178, license: '21E9UT'},
+{in: 1653828060403, out: 1653840973880, license: '6P93HI'},
+{in: 1653828180403, out: 1653847730496, license: 'F5YJIZ'},
+{in: 1653828240403, out: 1653831359280, license: '7CN4EN'},
+{in: 1653828240403, out: 1653840531563, license: '8N09GJ'},
+{in: 1653828300403, out: 1653857786578, license: '072EZ5'},
+{in: 1653828300403, out: 1653835616618, license: 'HSVS1T'},
+{in: 1653828480403, out: 1653855093306, license: 'GVKQ72'},
+{in: 1653828480403, out: 1653846990017, license: '8BN6PJ'},
+{in: 1653828660403, out: 1653837759309, license: '27Y8FZ'},
+{in: 1653828720403, out: 1653836552036, license: 'BNYN2Z'},
+{in: 1653828720403, out: 1653852731581, license: 'AIDWMI'},
+{in: 1653828780403, out: 1653837865282, license: 'KCULFF'},
+{in: 1653828840403, out: 1653841639954, license: 'UKQTQ6'},
+{in: 1653829080403, out: 1653844413376, license: '7WTANP'},
+{in: 1653829140403, out: 1653854585121, license: 'AWOKZV'},
+{in: 1653829200403, out: 1653857943524, license: 'Q0JVX2'},
+{in: 1653829380403, out: 1653855736835, license: 'K66Z3Q'},
+{in: 1653829500403, out: 1653832647635, license: 'II1PMQ'},
+{in: 1653829680403, out: 1653853520825, license: 'DZT11Q'},
+{in: 1653829740403, out: 1653834903239, license: '2QR00G'},
+{in: 1653829980403, out: 1653851793695, license: 'P2OXL9'},
+{in: 1653830040403, out: 1653843361078, license: 'BZFSY8'},
+{in: 1653830040403, out: 1653847748935, license: '5CMVBC'},
+{in: 1653830100403, out: 1653850325259, license: 'X0H4BO'},
+{in: 1653830100403, out: 1653835537556, license: 'TA40CH'},
+{in: 1653830220403, out: 1653833095486, license: 'WLSRYF'},
+{in: 1653830340403, out: 1653852442939, license: 'TND5K9'},
+{in: 1653830340403, out: 1653838643106, license: 'UFD42T'},
+{in: 1653830460403, out: 1653843365057, license: 'B0N6G8'},
+{in: 1653830580403, out: 1653837499195, license: 'QL5M95'},
+{in: 1653830640403, out: 1653855124970, license: 'MB85I2'},
+{in: 1653830640403, out: 1653845564698, license: 'WK2ART'},
+{in: 1653830700403, out: 1653856593215, license: 'OENNCP'},
+{in: 1653830700403, out: 1653856080317, license: 'ESXV8M'},
+{in: 1653830820403, out: 1653843665793, license: '9GRZGP'},
+{in: 1653830880403, out: 1653830961249, license: 'AO3X0U'},
+{in: 1653830880403, out: 1653846689941, license: 'GF4R16'},
+{in: 1653830940403, out: 1653838964488, license: 'H9H37J'},
+{in: 1653831000403, out: 1653851166174, license: 'Y21WUW'},
+{in: 1653831000403, out: 1653856607499, license: '8OIRL3'},
+{in: 1653831060403, out: 1653831127669, license: '3SGNYN'},
+{in: 1653831120403, out: 1653855086707, license: 'JEF1BE'},
+{in: 1653831240403, out: 1653837230187, license: 'FZNX11'},
+{in: 1653831300403, out: 1653847242129, license: 'NSNYJR'},
+{in: 1653831480403, out: 1653845973086, license: 'J1Q4G1'},
+{in: 1653831540403, out: 1653850006860, license: 'E2GOAN'},
+{in: 1653831600403, out: 1653842402150, license: '0FF3CJ'},
+{in: 1653831600403, out: 1653839704376, license: '8GXIOG'},
+{in: 1653831660403, out: 1653850808864, license: '4CUUPU'},
+{in: 1653831660403, out: 1653856903325, license: 'BALPWD'},
+{in: 1653831780403, out: 1653839886224, license: 'PFHQH5'},
+{in: 1653831900403, out: 1653847553206, license: 'E3O2CL'},
+{in: 1653832020403, out: 1653840098289, license: 'UP583T'},
+{in: 1653832020403, out: 1653841211207, license: 'D885U3'},
+{in: 1653832080403, out: 1653836071834, license: 'DEM8J9'},
+{in: 1653832080403, out: 1653850435559, license: '5FWNBI'},
+{in: 1653832140403, out: 1653847476814, license: 'EG5N86'},
+{in: 1653832260403, out: 1653844673402, license: '50E6OK'},
+{in: 1653832260403, out: 1653855208364, license: '1E5JEF'},
+{in: 1653832320403, out: 1653855539240, license: '4HOMRM'},
+{in: 1653832380403, out: 1653841682252, license: 'C23Q9U'},
+{in: 1653832500403, out: 1653854235981, license: 'ZTBQBZ'},
+{in: 1653832560403, out: 1653842161573, license: 'UK9NZV'},
+{in: 1653832680403, out: 1653837888563, license: 'C7I41F'},
+{in: 1653832680403, out: 1653846178725, license: 'X2FJ41'},
+{in: 1653832800403, out: 1653835153625, license: 'O3260W'},
+{in: 1653832800403, out: 1653845030413, license: 'HT1SWQ'},
+{in: 1653832920403, out: 1653835354166, license: 'HB5YTR'},
+{in: 1653832920403, out: 1653848955772, license: 'IBAI8Q'},
+{in: 1653832980403, out: 1653842162631, license: 'UCRGZ7'},
+{in: 1653833100403, out: 1653857314419, license: 'YXS7CF'},
+{in: 1653833220403, out: 1653852534875, license: '7ZHVMS'},
+{in: 1653833280403, out: 1653837009215, license: 'D3I95W'},
+{in: 1653833400403, out: 1653846671306, license: 'IJXNNR'},
+{in: 1653833400403, out: 1653857210453, license: 'PMBIVW'},
+{in: 1653833460403, out: 1653857636397, license: 'ZR5MIL'},
+{in: 1653833460403, out: 1653843014079, license: 'KI69TJ'},
+{in: 1653833580403, out: 1653834805752, license: 'BRJOT3'},
+{in: 1653833580403, out: 1653849905896, license: '5INB69'},
+{in: 1653833640403, out: 1653848999944, license: 'L3VKQR'},
+{in: 1653833640403, out: 1653841063959, license: 'R1LEGM'},
+{in: 1653833760403, out: 1653844189421, license: 'PIPO0K'}            
 
 ]
-
-console.log("Full entry:", entries)
 
 // mapping over JSON data to manipulate data and create a formatted array saved to a var called entryData.
 
 const entryData = entries.map(entry => {
 
-// Using the DATE() method to configure correct date/time in/out display
-
-  const In = new Date(entry.in)
-  const Out = new Date(entry.out)
-  const timeIn = `
-    ${In.toLocaleDateString()} 
-    ${In.toLocaleTimeString("en-us", {timeStyle: "medium"})}
-    `
-  const timeOut = `
-    ${Out.toLocaleDateString()} 
-    ${Out.toLocaleTimeString("en-us", {timeStyle: "medium"})}
-    `
-// Duration spent in parking
-
-  entry.rawDuration = entry.out-entry.in
-  entry.duration = Math.round(
-    (entry.rawDuration / 1000 / 60 / 60) * 100) / 100
-    
-// Fee for duration
-
-  let price = entry.price = entry.duration <= 1 ? "free": 
-    Math.ceil(entry.duration * 2.99 * 100) / 100
-
-//returning the array values within table elements concatted to populate the tbody element in HTML doc.
-  return (
-    
-    `<tr>
-    <td>${entry.license}</td>
-    <td id="price">$ ${entry.price}</td>
-    <td>${entry.duration}</td>
-    <td>${timeIn}</td>
-    <td>${timeOut}</td>
-</tr>`
-  )   
-}).join('')
-
-const tabelBody = document.querySelector("#tableBody")
-tableBody.innerHTML = entryData;
-
-function priceColor (){
-
-  if(document.getElementById("price").innerText === "free"){
-    document.getElementById("price").childNodes.style.color = "blue"
-  }else{
-    document.getElementById("price").childNodes.style.color = "red"
-  }
-
-}
-
-
-priceColor()
-  // if (price === "free"){
-  // priceColor.style.color = "blue"
-  // }else if (duration < 24){
-  // priceColor.style.color = "red"
-  // }
-
-
-
-
-//api call
+  // Using the DATE() method to configure correct date/time in/out display of time stamp entries
   
-//   fetch('http://localhost:4200/api/event/')
-//   .then(res => res.json())
-//   .then(entries => {
+    const In = new Date(entry.in)
+    const Out = new Date(entry.out)
+    const timeIn = `
+      ${In.toLocaleDateString()} 
+      ${In.toLocaleTimeString("en-us", {timeStyle: "medium"})}
+      `
+    const timeOut = `
+      ${Out.toLocaleDateString()} 
+      ${Out.toLocaleTimeString("en-us", {timeStyle: "medium"})}
+      `
+  // Duration spent in parking
 
+  let entryTime = Math.abs(entry.out-entry.in) / 36e5;
+  const duration = entryTime.toFixed(1)
 
-// })
-
-// NOTES: STILL UNSOLVED:
-// 1. Return color-coded entries for ("free" = blue and over 24h = blue) 
-// 2. Date format need adjustment to show am/pm and not time zone
-
-                              
+  // Fee for duration
+  
+  // the "price" variable returns either "free" for entries less than 1h
+  //or the $ price p/h using the ternary operator
+    const price = duration <= 1 ? "FREE": 
+     Math.ceil(duration * 2.99 * 100) / 100
+  
+  // the priceTD variable returns the correctly foramtted td element according to "free"/1-24h and over 24h
+    const pricdTD = price === "FREE" ? `<td id="td-price-free">${price}</td>` :  
+    duration >23.9 ? `<td id="td-price-24h">${price}</td>` : 
+    `<td>${price}</td>`
+  
+  //returning the HTML <tr> element with <td> elements populated with the pertinent values derived from the entryData array.
+    return (
+      
+      `
+      <tr>
+        <td>${entry.license}</td>
+        ${pricdTD}
+        <td>$ ${duration}</td>
+        <td>${timeIn}</td>
+        <td>${timeOut}</td>
+      </tr>
+  `
+    )   
+  }).join('')
+  
+  const tabelBody = document.querySelector("#tableBody")
+  tableBody.innerHTML = entryData
+  
+  //api call
+    
+  //   fetch('http://localhost:4200/api/event/')
+  //   .then(res => res.json())
+  //   .then(entries => {
+  
+  
+  // })                             
