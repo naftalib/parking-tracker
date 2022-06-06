aac396a26d09a890a45d82db9029b454dc3f94b7
naftalib <45719650+naftalib@users.noreply.github.com>
2022-06-06 22:23:59 +0300

    final 1

--- client/js/app.js
+++ client/js/app.js
@@ -209,14 +209,17 @@ const entries = [
 
 ]
 
-// mapping over JSON data to manipulate data and create a formatted array saved to a var called entryData.
+// ----------------------TABLE display logic------------------------------------
+
+// 1. Taking JSON obj to create a formatted array saved to a var called 'entryData'.
 
 const entryData = entries.map(entry => {
 
-  // Using the DATE() method to configure correct date/time in/out display of time stamp entries
+  // Using DATE() method to configure correct date/time in/out display of time stamps.
   
     const In = new Date(entry.in)
     const Out = new Date(entry.out)
+
     const timeIn = `
       ${In.toLocaleDateString()} 
       ${In.toLocaleTimeString("en-us", {timeStyle: "medium"})}
@@ -225,12 +228,12 @@ const entryData = entries.map(entry => {
       ${Out.toLocaleDateString()} 
       ${Out.toLocaleTimeString("en-us", {timeStyle: "medium"})}
       `
-  // Duration spent in parking
+  // Total parking duration calculation
 
-  let entryTime = Math.abs(entry.out-entry.in) / 36e5;
+  let entryTime = Math.abs(entry.in-entry.out) / 36e5;
   const duration = entryTime.toFixed(1)
 
-  // Fee for duration
+  // Fee for respective duration
   
   // the "price" variable returns either "free" for entries less than 1h
   //or the $ price p/h using the ternary operator
@@ -260,21 +263,63 @@ const entryData = entries.map(entry => {
   const tabelBody = document.querySelector("#tableBody")
   tableBody.innerHTML = entryData
 
-  //Need math logic on array to display average between all entries
+  //---------------------SUMARY SECTION--------------------
+  
+  // Average duration of all entries
+
+  // Logic applied:
+  // 1. Repeat of math to obtain the overall duration.
+  // 2. Since the .map function will return a string value need to coerce result to number value.
+  // 3. Average is calculated by adding each ittaration and deviding the result by the number of entries.  
 
   const smryDrtn = entries.map(entry=>{
 
     let entryTime = Math.abs(entry.out-entry.in) / 36e5;
     const duration = entryTime.toFixed(1)
-    parseInt(duration)
-    return duration
+    return parseInt(duration)
   })
-  // const sum = smryDrtn.reduce((a, b) => a + b, 0)
-  // const avg = (sum / smryDrtn.length)
-
- const avgDrtn = smryDrtn.reduce((a,b) => (a+b)) / smryDrtn.length
-console.log(smryDrtn)
+  const avgDrtn = smryDrtn.reduce((a,b) => (a+b)) / smryDrtn.length
   
+document.querySelector('#avr').textContent = `${avgDrtn.toFixed(1)} h`
+
+// Average all entries
+
+document.querySelector("#total").textContent = entries.length
+
+// Average Free entries
+
+//Filtered the array of total duration of each entry according to those less than 1 hour.
+
+const smryFree = entries.map(entry=>{
+
+  let entryTime = Math.abs(entry.out-entry.in) / 36e5;
+  const duration = entryTime.toFixed(1)
+
+  return duration
+})
+.filter(entry=> entry <= 1 ? true : false)
+
+document.querySelector("#free").textContent = smryFree.length
+
+// Average entries over 24h
+
+//Same logic as with free entries
+// *** NOTE: Not worth refactoring the code with these two functions so as to apply 
+// shared logic between them because the additional logic required over complicates things.
+
+const smry24 = entries.map(entry=>{
+
+  let entryTime = Math.abs(entry.out-entry.in) / 36e5;
+  const duration = entryTime.toFixed(1)
+
+  return duration
+})
+.filter(entry=> entry > 23.9 ? true : false)
+
+document.querySelector("#_24").textContent = smry24.length
+
+console.log(smry24.length)
+
   //api call
     
   //   fetch('http://localhost:4200/api/event/')
@@ -282,4 +327,4 @@ console.log(smryDrtn)
   //   .then(entries => {
   
   
-  // })                             
+  // })          
